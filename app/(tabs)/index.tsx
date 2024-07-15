4d310fa97d4933571f9c73ca53f28ab9bd7f67fd
zhengyi Ma <zbncsyy@gmail.com>
2024-07-15 23:14:15 +0800

    feat: hello world

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,27 @@
 import { StyleSheet, View } from "react-native";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { Canvas, Circle, Group } from "@shopify/react-native-skia";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  return <View></View>;
+  const width = 256;
+  const height = 256;
+  const r = width * 0.33;
+  return (
+    <SafeAreaView>
+      <Canvas style={styles.canvas}>
+        <Group blendMode={"multiply"}>
+          <Circle cx={r} cy={r} r={r} color="cyan" />
+          <Circle cx={width - r} cy={r} r={r} color="magenta" />
+          <Circle cx={width / 2} cy={width - r} r={r} color="yellow" />
+        </Group>
+      </Canvas>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+  canvas: {
+    width: 256,
+    height: 256,
   },
 });
